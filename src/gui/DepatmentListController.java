ca2e6a5330161e26acef4d91312d70a75980087d
Danilo Alvim <danilo.alvim@hotmail.com>
2021-04-14 17:03:59 -0300

    Department comboBox

--- src/gui/DepatmentListController.java
+++ src/gui/DepatmentListController.java
@@ -111,6 +111,7 @@ public class DepatmentListController implements Initializable, DataChangeListene
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.showAndWait();
 		} catch (IOException e) {
+			e.printStackTrace();
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
